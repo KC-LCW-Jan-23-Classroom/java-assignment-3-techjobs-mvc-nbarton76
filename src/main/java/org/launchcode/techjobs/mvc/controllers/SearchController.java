c1a5eddde4219ca8ba0c3311405dd6bdb5ef4d28
Nichelle Barton <52866856+nbarton76@users.noreply.github.com>
2023-08-13 16:26:23 -0500

    corrected handler

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -21,9 +22,10 @@ public class SearchController {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
-    @PostMapping(value = "results")
-    public String displaySearchResults(Model model, String searchType, String searchTerm) {
-        if (searchType.equals("all")) {
+    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        if (searchTerm.equals("all")) {
             model.addAttribute("jobs", ListController.columnChoices);
             model.addAttribute("title", "All Jobs");
         } else {
@@ -32,6 +34,6 @@ public class SearchController {
         }
         return "search";
     }
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+
 
 }
