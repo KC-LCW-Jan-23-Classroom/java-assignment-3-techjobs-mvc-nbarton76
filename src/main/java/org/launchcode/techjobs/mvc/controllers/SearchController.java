c613606e884bd24de941715cc3187de05802d04c
Nichelle Barton <52866856+nbarton76@users.noreply.github.com>
2023-08-13 15:42:36 -0500

    Added handler

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
@@ -20,7 +21,17 @@ public class SearchController {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
-
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, String searchType, String searchTerm) {
+        if (searchType.equals("all")) {
+            model.addAttribute("jobs", ListController.columnChoices);
+            model.addAttribute("title", "All Jobs");
+        } else {
+            model.addAttribute("jobs", ListController.columnChoices);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        return "search";
+    }
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
 }
